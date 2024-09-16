bca7a6c877fd03fa319c11e06ccd10c95e501e43
lucasalonia <salonialuca97@gmail.com>
2024-09-16 17:08:08 -0300

    Initial commit

---
+++ src/clases/Producto.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author salon
+ */
+public class Producto {
+    private String categoria;
+    private String nombre;
+    private int precio;
+
+    public Producto( String nombre,String categoria, int precio) {
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
+}
